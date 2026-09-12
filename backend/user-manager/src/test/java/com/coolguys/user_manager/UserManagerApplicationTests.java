93116cc10b5f99021cf8550134155d2ccc70a9f0
Roman Sukharev <sukharevr@list.ru>
2026-09-12 19:51:33 +0300

    [TSK-01] Реализован user-manager

---
+++ backend/user-manager/src/test/java/com/coolguys/user_manager/UserManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coolguys.user_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
