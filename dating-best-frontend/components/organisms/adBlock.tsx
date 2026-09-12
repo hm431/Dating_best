6738a38951adb0a6abcb05f546bd99be03eacd05
Андрей Чучалин <andrej@MacBook-Air-Andrej.local>
2026-09-12 22:38:07 +0300

    TSK-1.3. - Верстка Блока для вас  fixes

--- dating-best-frontend/components/organisms/adBlock.tsx
+++ dating-best-frontend/components/organisms/adBlock.tsx
@@ -1,7 +1,7 @@
 import AdCard from "@/components/organisms/AdCard"
 
 export default function AdBlock() {
-    return (
+    return ( 
        <div className="w-full bg-[#181818B5] my-4 h-[90px] flex gap-3 overflow-y-auto scroll-hidden">
             <AdCard name="Маркет дизайна" herf="https://youtu.be/dQw4w9WgXcQ?si=vD4_1p9g9_ax00Un" isExternal={true} date={new Date(2025, 9, 19)} price="Вход свободный"/>
             <AdCard name="Маркет дизайна" herf="https://youtu.be/dQw4w9WgXcQ?si=vD4_1p9g9_ax00Un" isExternal={true} date={new Date(2025, 9, 19)} price="Вход свободный"/>
